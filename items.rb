4495dd30efd55fc8a9c667566757902c4efceb7f
Andrew Benz <andorbal@gmail.com>
2012-09-27 22:13:21 -0500

    Built the logic for creating new items

--- items.rb
+++ items.rb
@@ -14,7 +14,22 @@ get '/cat/:id/items' do
 end
 
 post '/cat/:id/item' do
-  
+  db = Database.new
+  cat = db.cats.find_one _id: BSON::ObjectId(params[:id])
+
+  item = {
+    _id: BSON::ObjectId.new,
+    name: params["name"],
+    cat: { _id: cat["_id"], name: cat["name"] },
+    fields: []
+  }
+
+  (cat["fields"] || []).each do |field|
+    item["fields"] << { _id: field["_id"], name: field["name"], value: params["field_" + field["_id"]] }
+  end
+
+  db.items.insert item
+  redirect "/cat/" + cat["_id"].to_s + "/items"
 end
 
 post '/item/:id' do
